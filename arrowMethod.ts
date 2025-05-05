0ab1910c85e31018bfee6ea54af543f2edc3e897
Parwinder Singh <bhanguparwindersingh31@gmail.com>
2025-05-05 12:58:33 +0530

    arrowMethod

---
+++ arrowMethod.ts
@@ -0,0 +1,42 @@
+
+
+interface Produce{
+
+    id : number,
+    name : string,
+    price : number,
+    inStock : boolean
+}
+
+let prod : Produce[] = [
+    {
+  id : 1,
+  name: "userOne",
+  price: 1,
+  inStock: true
+},
+{
+    id : 2,
+    name : "userTwo",
+    price : 2,
+    inStock : false
+},
+{
+    id : 3,
+    name: "userThree",
+    price : 3,
+    inStock : true
+}
+]
+
+function getAvailableProducts(products: Produce[]): Produce[] {
+
+    return products.filter(product=>product.inStock)
+
+    
+}
+
+
+const availableProds = getAvailableProducts(prod)
+
+console.log(availableProds)
